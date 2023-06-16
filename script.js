e28a988294725ff385bf4591048557f321f99db3
Dinesh_Reddy <119753519+Dinesh-7075@users.noreply.github.com>
2023-06-16 16:53:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 // complete the given function
 
 function palindrome(str){
+     let rev = "";
+        str = str.toLowerCase();
+        // str = str.replace(" " , "");
+        str = str.replace(/ |'|,|;|:/gi, "");
+        // console.log(str);
+//         str = str.replace("'" , "");
+//         str = str.replace("." , "");
+// 		str = str.replace("?" , "");
+// 		str = str.replace(";" , "");
+// 		str = str.replace(":" , "");
+        let l = str.length;
+        let i = 0; 
+        let j = (l-1);
+        while(i<=j) {
+            if(str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+  let ans = palindrome("a man, a plan, a canal Panama");
+  console.log(ans);
 
-}
 module.exports = palindrome
